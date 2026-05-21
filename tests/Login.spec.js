960075aa6c24d4bb5e582aefdda41f574043247b
Akshaya19.n-max <akshaya19.n@gmail.com>
2026-05-21 10:51:37 +0530

    changes

---
+++ tests/Login.spec.js
@@ -0,0 +1,9 @@
+import { test } from "@playwright/test";
+import LoginPage from "../pages/LoginPage.js";
+
+test("Login test", async ({ page }) => {
+
+    const login = new LoginPage(page);
+    await login.launchURL();
+    await login.loginToPage("rahulshettyacademy", "Learning@830$3mK2");
+})
