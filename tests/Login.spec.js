f816f6f375a6da5aab544bbc9f4a5971a8d4ed41
Akshaya19.n-max <akshaya19.n@gmail.com>
2026-05-22 20:09:15 +0530

    new commit

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -3,6 +3,10 @@ import LoginPage from "../pages/LoginPage.js";
 
 test("@smoke Login test ", async ({ page }) => {
     const login = new LoginPage(page);
+
     await login.launchURL();
+
     await login.loginToPage("rahulshettyacademy", "Learning@830$3mK2");
+
+    //jenkins feature-branch
 })
