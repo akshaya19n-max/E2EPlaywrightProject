3b0963022c4f8c8a08db1c1201a24bc7d245f18a
Akshaya19.n-max <akshaya19.n@gmail.com>
2026-05-22 21:52:56 +0530

    new commit

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -1,7 +1,7 @@
 import { test } from "@playwright/test";
 import LoginPage from "../pages/LoginPage.js";
 
-test("@smoke Login test ", async ({ page }) => {
+test("@smoke @regression Login test ", async ({ page }) => {
     const login = new LoginPage(page);
 
     await login.launchURL();
